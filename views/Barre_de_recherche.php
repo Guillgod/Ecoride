53c713f85c0f3360b2a95e101c8ae406545bcdaa
Guillgod <guillaume.jobin.buffard@gmail.com>
2025-06-11 20:41:55 +0200

    changement disposition barre de recherche

--- views/Barre_de_recherche.php
+++ views/Barre_de_recherche.php
@@ -1,4 +1,4 @@
-<?php
+<div?php
 if (session_status() == PHP_SESSION_NONE) {
     session_start(); 
 }
@@ -28,15 +28,24 @@ if (session_status() == PHP_SESSION_NONE) {
         <form action="../controllers/Handle_Search_Controller.php" method="post">
             <input type="hidden" name="form_type" value="Barre_de_recherche.php">
             <div class="form-recherche">
-                
-                <li><input type="text" id="lieu_depart" name="lieu_depart" required minlength="1" maxlength="50" size="20" placeholder="Départ"  value="<?= isset($_SESSION['form_data']['lieu_depart']) ? htmlspecialchars($_SESSION['form_data']['lieu_depart']) : '' ?>"></li>
-                
-                
-                <li><input type="text" id="lieu_arrivee" name="lieu_arrivee" required minlength="1" maxlength="50" size="20" placeholder="Destination" value="<?= isset($_SESSION['form_data']['lieu_arrivee']) ? htmlspecialchars($_SESSION['form_data']['lieu_arrivee']) : '' ?>"></li>
+                <div class="form-recherche-content">
                     
+                <li><label for="lieu_depart">Départ :</label>
+                <li><input type="text" id="lieu_depart" name="lieu_depart" required minlength="1" maxlength="50" size="20" placeholder="Paris"  value="<?= isset($_SESSION['form_data']['lieu_depart']) ? htmlspecialchars($_SESSION['form_data']['lieu_depart']) : '' ?>"></li>
                 
+                </div>
+
+                <div class="form-recherche-content">
+                    
+                <li><label for="lieu_arrivée">Destination :</label>
+                <li><input type="text" id="lieu_arrivee" name="lieu_arrivee" required minlength="1" maxlength="50" size="20" placeholder="Lyon" value="<?= isset($_SESSION['form_data']['lieu_arrivee']) ? htmlspecialchars($_SESSION['form_data']['lieu_arrivee']) : '' ?>"></li>
+                </div>
+                
+                <div class="form-recherche-content">
+                    
+                <li><label for="date_depart">Date :</label>
                 <li><input type="date" id="date_depart" name="date_depart" required minlength="4" maxlength="8" size="10"  value="<?= isset($_SESSION['form_data']['date_depart']) ? htmlspecialchars($_SESSION['form_data']['date_depart']) : '' ?>"></li>
-                       
+                </div>
 
 
                 <button class="button" type="submit">Rechercher</button>
